dadb45b59e9be05cb6efe31a941a043924d7e21c
Pl@yer <skibiditoilet6775@gmail.com>
2026-02-12 20:55:24 +0100

    Updated Functions

--- src/engine/sceneio.cpp
+++ src/engine/sceneio.cpp
@@ -2,21 +2,18 @@
 /* from: engine:sceneio.cpp
    addr: 00582340 */
 
-void __fastcall sioSetSourceFile(fioFILE *param_1)
+void sioSetSourceFile(fioFILE *param_1)
 
 {
-  _sioFilePtr = param_1;
+  sioFilePtr = file;
   (**(code **)(*(int *)param_1 + 0xc))(0,1);
   return;
 }
 
-
-
-
 /* from: engine:sceneio.cpp
    addr: 00582350 */
 
-int __fastcall sioReadScene(scnSCENE *param_1)
+int sioReadScene(scnSCENE *param_1)
 
 {
   int iVar1;
@@ -34,38 +31,38 @@ int __fastcall sioReadScene(scnSCENE *param_1)
   uStack_c = 0xffff;
   local_a = 0xffffffff;
   local_6 = 0xffffffff;
-  iVar1 = (**(code **)(*(int *)_sioFilePtr + 0x10))();
+  iVar1 = (**(code **)(*(int *)sioFilePtr + 0x10))();
   do {
-    if (((iVar1 != 0) || (iVar1 = fioFILE::ReadChunk(_sioFilePtr,(fioCHUNK *)&local_10), iVar1 == 0)
-        ) || (iVar1 = (**(code **)(*(int *)_sioFilePtr + 0x10))(), iVar1 != 0)) {
-      apFree(_sioTexListNmb);
-      _sioTexListNmb = (int *)0x0;
+    if (((iVar1 != 0) || (iVar1 = fioFILE::ReadChunk(sioFilePtr,(fioCHUNK *)&local_10), iVar1 == 0)
+        ) || (iVar1 = (**(code **)(*(int *)sioFilePtr + 0x10))(), iVar1 != 0)) {
+      apFree(sioTexListNmb);
+      sioTexListNmb = NULL;
       return 1;
     }
     fioFILE::RevertChunk(_sioFilePtr,(fioCHUNK *)0x0);
     if (local_10 < 0x21e) {
       if (local_10 == 0x21d) {
-        _sioReadNamedSelList((int *)(param_1 + 0x78),(selSEL **)(param_1 + 0x7c));
+        sioReadNamedSelList((int *)(param_1 + 0x78),(selSEL **)(param_1 + 0x7c));
       }
       else if (local_10 < 0x1b9) {
         if (local_10 == 0x1b8) {
-          _sioReadInstList(param_1);
+          sioReadInstList(param_1);
         }
         else if (local_10 == 0xf0) {
-          poVar2 = _sioReadObj();
+          poVar2 = sioReadObj();
           *(objOBJ **)(param_1 + 0x44) = poVar2;
         }
         else {
           if ((local_10 < 0x154) || (0x155 < local_10)) goto LAB_005824b8;
-          _sioReadTex();
+          sioReadTex();
         }
       }
       else if (local_10 == 0x1ea) {
-        _sioReadTplList(param_1);
+        sioReadTplList(param_1);
       }
       else {
         if (local_10 != 0x21c) goto LAB_005824b8;
-        pcVar3 = _sioReadColl_OLD();
+        pcVar3 = sioReadColl_OLD();
         *(cdtCOLL_SCN **)(param_1 + 0x50) = pcVar3;
         if (pcVar3 != (cdtCOLL_SCN *)0x0) {
           *(scnSCENE **)(pcVar3 + 100) = param_1;
@@ -74,18 +71,18 @@ int __fastcall sioReadScene(scnSCENE *param_1)
     }
     else if (local_10 < 0x281) {
       if (local_10 == 0x280) {
-        _sioReadLightList(param_1);
+        sioReadLightList(param_1);
       }
       else if (local_10 == 0x21e) {
         fioFILE::ReadChunk(_sioFilePtr,(fioCHUNK *)&local_10);
-        iVar1 = _sioReadPSheet((psSHEET **)(param_1 + 0x80));
+        iVar1 = sioReadPSheet((psSHEET **)(param_1 + 0x80));
         if (iVar1 == 0) {
-          apLogErr(s_Cannot_parse_global_scene_script);
+          apLogErr("Cannot parse global scene script");
         }
       }
       else {
         if (local_10 != 0x21f) goto LAB_005824b8;
-        pcVar3 = _sioReadColl();
+        pcVar3 = sioReadColl();
         *(cdtCOLL_SCN **)(param_1 + 0x50) = pcVar3;
         if (pcVar3 != (cdtCOLL_SCN *)0x0) {
           *(scnSCENE **)(pcVar3 + 100) = param_1;
@@ -93,7 +90,7 @@ int __fastcall sioReadScene(scnSCENE *param_1)
       }
     }
     else if (local_10 == 0x3ac) {
-      pcVar4 = _sioReadCamera();
+      pcVar4 = sioReadCamera();
       *(camCAMERA **)(param_1 + 0x84) = pcVar4;
     }
     else {
@@ -103,9 +100,6 @@ LAB_005824b8:
     iVar1 = (**(code **)(*(int *)_sioFilePtr + 0x10))();
   } while( true );
 }
-
-
-
 
 /* from: engine:sceneio.cpp
    addr: 00582510 */
@@ -125,98 +119,95 @@ int __fastcall sioReadSceneTex(void)
   uStack_c = 0xffff;
   local_a = 0xffffffff;
   local_6 = 0xffffffff;
-  iVar1 = (**(code **)(*(int *)_sioFilePtr + 0x10))();
-  while ((iVar1 == 0 && (iVar1 = fioFILE::ReadChunk(_sioFilePtr,(fioCHUNK *)&local_10), iVar1 != 0))
+  iVar1 = (**(code **)(*(int *)sioFilePtr + 0x10))();
+  while ((iVar1 == 0 && (iVar1 = fioFILE::ReadChunk(sioFilePtr,(fioCHUNK *)&local_10), iVar1 != 0))
         ) {
-    iVar1 = (**(code **)(*(int *)_sioFilePtr + 0x10))();
+    iVar1 = (**(code **)(*(int *)sioFilePtr + 0x10))();
     if (iVar1 != 0) break;
     if ((local_10 < 0x154) || (0x155 < local_10)) {
-      fioFILE::SkipChunk(_sioFilePtr,(fioCHUNK *)&local_10);
+      fioFILE::SkipChunk(sioFilePtr,(fioCHUNK *)&local_10);
     }
     else {
-      fioFILE::RevertChunk(_sioFilePtr,(fioCHUNK *)0x0);
+      fioFILE::RevertChunk(sioFilePtr,(fioCHUNK *)0x0);
       _sioReadTex();
     }
-    iVar1 = (**(code **)(*(int *)_sioFilePtr + 0x10))();
+    iVar1 = (**(code **)(*(int *)sioFilePtr + 0x10))();
   }
-  apFree(_sioTexListNmb);
-  _sioTexListNmb = (int *)0x0;
+  apFree(sioTexListNmb);
+  sioTexListNmb = NULL;
   return 1;
 }
-
-
-
 
 /* from: engine:sceneio.cpp
    addr: 005825D0 */
 
-int __fastcall sioReadTemplate(animTPL *param_1,int param_2)
+int sioReadTemplate(animTPL* tpl, bool loadTexture)
 
 {
-  int iVar1;
-  objOBJ *poVar2;
+  int result;
+  objOBJ* rootObject;
   
-  iVar1 = fioFILE::ReadChunk(_sioFilePtr,(fioCHUNK *)0x0);
-  if (iVar1 != 0x2e4) {
-    fioFILE::RevertChunk(_sioFilePtr,(fioCHUNK *)0x0);
+  result = fioFILE::ReadChunk(sioFilePtr, NULL);
+  if (result != 0x2e4) {
+    fioFILE::RevertChunk(sioFilePtr, NULL);
     return 0;
   }
-  iVar1 = fioFILE::ReadChunk(_sioFilePtr,(fioCHUNK *)0x0);
-  if (iVar1 != 0x2e5) {
+  result = fioFILE::ReadChunk(sioFilePtr, NULL);
+  if (result != 0x2e5) {
     return 0;
   }
-  (**(code **)(*(int *)_sioFilePtr + 0x14))(param_1,4,4);
-  fioFILE::Read(_sioFilePtr,(char *)(param_1 + 4),0x40);
-  fioFILE::Read(_sioFilePtr,(char *)(param_1 + 0x44),0x40);
-  if (*(int *)param_1 != 0x4154504c) {
+  sioFilePtr->Read(&tpl->magic, 4);
+  fioFILE::Read(sioFilePtr, tpl->templateName, 0x40);
+  fioFILE::Read(sioFilePtr, tpl->className, 0x40);
+   
+  if (tpl->magic != ATPL) { // magic header for TPL format (0x4154504c)
     return 0;
   }
-  if (param_2 != 0) {
-    iVar1 = _sioReadTex();
-    if (iVar1 == 0) {
+  if (loadTextures != 0) {
+    result = sioReadTex();
+    if (result == 0) {
       return 0;
     }
   }
-  poVar2 = _sioReadObj();
-  *(objOBJ **)(param_1 + 0x88) = poVar2;
-  if (poVar2 == (objOBJ *)0x0) {
+  rootObject = sioReadObj();
+  tpl->rootObject = rootObject;
+  if (rootObject == NULL) {
     return 0;
   }
-  iVar1 = _sioReadObjSkinList(param_1);
-  if (iVar1 == 0) {
+  result = sioReadObjSkinList(tpl);
+  if (result == 0) {
     return 0;
   }
-  iVar1 = _sioReadObjAnimList(param_1);
-  if (iVar1 == 0) {
+  result = sioReadObjAnimList(tpl);
+  if (result == 0) {
     return 0;
   }
-  iVar1 = _sioReadAnimSeq(param_1);
-  if (iVar1 == 0) {
+  result = sioReadAnimSeq(tpl);
+  if (result == 0) {
     return 0;
   }
-  iVar1 = _sioReadBVolList(param_1);
-  if (iVar1 == 0) {
+  result = sioReadBVolList(tpl);
+  if (result == 0) {
     return 0;
   }
-  iVar1 = _sioReadPSheet((psSHEET **)(param_1 + 0xc4));
-  if (iVar1 == 0) {
-    apLogErr(s_Cannot_parse_script_for_template,param_1 + 4);
+  result = sioReadPSheet(&tpl->propertySheet); // 0xC4 
+  if (result == 0) {
+    apLogErr("Cannot parse script for template: %s", tpl->templateName);
   }
-  iVar1 = _sioReadNamedSelList((int *)(param_1 + 0xbc),(selSEL **)(param_1 + 0xc0));
-  if (iVar1 == 0) {
+  result = sioReadNamedSelList(&tpl->numSelections, &tpl->selectionList); // 0xB4, 0xC0
+  if (result == 0) {
     return 0;
   }
-  iVar1 = _sioReadLastChunk();
-  if (iVar1 == 0) {
+  result = sioReadLastChunk();
+  if (result == 0) {
     return 0;
   }
-  if (param_2 != 0) {
-    apFree(_sioTexListNmb);
-    _sioTexListNmb = (int *)0x0;
+  if (loadTextures != 0) {
+    apFree(sioTexListNmb);
+    sioTexListNmb = NULL;
   }
-  animCleanConstSpl(param_1);
-  animCompressAnim(param_1);
-  animCleanConstSpl(param_1);
+  animCleanConstSpl(tpl);
+  animCompressAnim(tpl);
+  animCleanConstSpl(tpl);
   return 1;
 }
-
